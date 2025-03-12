3ae813017eb285421e2f682323b94c3682a00364
deepa mammen <deepa090590@gmail.com>
2025-03-12 13:21:28 -0300

    10k plus tweets sent and processed in 5 minutes

--- src/ingest/ingest_tweets.py
+++ src/ingest/ingest_tweets.py
@@ -66,15 +66,15 @@ try:
         elapsed_time = time.time() - start_time
         if elapsed_time >= CHECK_INTERVAL:
             tweets_per_minute = tweet_count / (elapsed_time / 60)
-            logger.info(f"Processed {tweet_count} tweets in {elapsed_time:.1f} seconds( ~{tweets_per_minute:.1f} tweets/minute)")
+            logger.info(f"Sent {tweet_count} tweets in {elapsed_time:.1f} seconds( ~{tweets_per_minute:.1f} tweets/minute)")
 
             #Reset counters
             tweet_count = 0
             start_time = time.time()
 
-        time.sleep(random.uniform(0.1, 1.0)) # to simulate real-time flow
+        time.sleep(0.01) # to achieve 10K + tweets
 except KeyboardInterrupt:
     elapsed_time = time.time() - start_time
     tweets_per_minute = tweet_count / (elapsed_time / 60) if elapsed_time > 0 else 0
 
-    logger.info(f"Stopped mock tweet stream. Processed {tweet_count} tweets in {elapsed_time:.1f} seconds (~{tweets_per_minute:.1f} tweets/minute)")
+    logger.info(f"Stopped mock tweet stream. Sent {tweet_count} tweets in {elapsed_time:.1f} seconds (~{tweets_per_minute:.1f} tweets/minute)")
