f28d82c77472675214f180fe0978425567f1b2c9
deepa mammen <deepa090590@gmail.com>
2025-03-14 16:41:12 -0300

    Setup.sh changes

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
-mkdir -p ~/.streamlit/ && echo "[server]\nheadless = true\nport = $PORT\nenableCORS = false\n" > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/
+echo "[server]" > ~/.streamlit/config.toml
+echo "headless = true" >> ~/.streamlit/config.toml
+echo "port = \$PORT" >> ~/.streamlit/config.toml
+echo "enableCORS = false" >> ~/.streamlit/config.toml
