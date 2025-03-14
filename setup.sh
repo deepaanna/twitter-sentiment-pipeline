f9250247e1ea2da639d625c258541650f308bf1e
deepa mammen <deepa090590@gmail.com>
2025-03-14 14:48:29 -0300

    Changing some set up for heroku

--- setup.sh
+++ setup.sh
@@ -1,5 +1,1 @@
-mkdir -p ~/.streamlit/
-echo "[server]" > ~/.streamlit/config.toml
-echo "headless = true" >> ~/.streamlit/config.toml
-echo "port = \$PORT" >> ~/.streamlit/config.toml
-echo "enableCORS = false" >> ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/ && echo "[server]\nheadless = true\nport = $PORT\nenableCORS = false\n" > ~/.streamlit/config.toml
